fa0b55780bfce914f7d03389f96cde99a40428c7
Abbas <139627415+ItsNotAbbas@users.noreply.github.com>
2026-05-03 17:48:07 +0100

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cs
@@ -0,0 +1,22 @@
+public class Solution 
+{
+    public int[] CountBits(int n) 
+    {
+        int[] res = new int[n + 1];
+
+        for (int i = 0; i <= n; i++)
+        {
+            int count = 0;
+            int temp = i;
+            
+            while (temp != 0)
+            {
+                count += temp % 2;
+                temp = temp >> 1;
+            }
+
+            res[i] = count;
+        }
+        return res;
+    }
+}
